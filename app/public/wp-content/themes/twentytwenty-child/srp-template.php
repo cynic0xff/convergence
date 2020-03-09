aac6b6ae1572cde2e702060f110e457aacbe3f07
Scott Wren <cynics0xff@gmail.com>
2020-03-09 17:36:47 -0400

    Add vdp basic template

--- app/public/wp-content/themes/twentytwenty-child/srp-template.php
+++ app/public/wp-content/themes/twentytwenty-child/srp-template.php
@@ -8,4 +8,25 @@
  * @since 1.0
  */
 
+global $wpdb;
+
+//create the sql structure
+$table_name = 'inventory';
+$column_names = 'id, vin, stock_number, make, model, year_manufactured';
+$sql = 'SELECT ' . $column_names . ' FROM ' . $table_name;
+
+//execute the query
+$vehicles = $wpdb->get_results($sql);
+
+//loop through the array
+foreach($vehicles as $v) {
+    echo $v->id;
+    echo $v->vin;
+    echo $v->make;
+    echo $v->model;
+    echo $v->year_manufactured;
+}
+
+
 get_template_part( 'singular' );
+?>
---
+++ app/public/wp-content/themes/twentytwenty-child/vdp-template.php
@@ -0,0 +1,20 @@
+<?php
+ /*
+ Template Name: Full Width
+ */
+
+get_header(); ?>
+
+
+<div id="primary" class="content-area sixteen columns">
+		<main id="main" class="site" role="main">
+
+				<?php get_template_part( 'content/content', 'page' ); ?>
+
+                <h1>WHAT</h1>
+        </main>
+</div>
+
+<?php
+get_footer();
+?>
